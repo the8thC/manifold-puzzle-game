1a40ba1a8748f59fc55a9ff5df86f1aa6f0bb7aa
Eugeniy Gusev <eugeniy.gusev@devexpress.com>
2021-05-03 23:51:09 +0300

    check view angle before teleportation

--- Assets/Teleport Trigger/Scripts/PlayerTeleport.cs
+++ Assets/Teleport Trigger/Scripts/PlayerTeleport.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTeleport : MonoBehaviour
-{
+public class PlayerTeleport : MonoBehaviour {
     public PlayerTeleport destination;
-    void OnTriggerEnter(Collider other) {
+    bool CheckAngles(Collider other) {
+        var yAngle = other.transform.eulerAngles.y;
+        if(yAngle >= -1 && yAngle <= 91)
+            return true;
+        if(yAngle >= -269 && yAngle <= -181)
+            return true;
+        return false;
+    }
+    void OnTriggerStay(Collider other) {
         if(destination == null || other.tag != "Player")
+            return;
+        if(!CheckAngles(other))
             return;
         var positionDiff = transform.position - other.transform.position;
         other.transform.position = destination.transform.position - positionDiff;
