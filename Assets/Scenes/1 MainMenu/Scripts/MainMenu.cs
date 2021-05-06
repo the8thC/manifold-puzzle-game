f7abd2f5c9f9d80ea5b44e1a6b47ee538ce45a3f
Eugeniy Gusev <eugeniy.gusev@devexpress.com>
2021-05-06 14:37:20 +0300

    add load and play menus

--- Assets/Scenes/1 MainMenu/Scripts/MainMenu.cs
+++ Assets/Scenes/1 MainMenu/Scripts/MainMenu.cs
@@ -1,17 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void QuitGame() {
         Application.Quit();
-    }
-
-    public void Play() {
-        SceneManager.LoadScene(1);
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
-        SceneManager.UnloadSceneAsync(0);
     }
 }
---
+++ Assets/Scenes/1 MainMenu/Scripts/PlayMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+    public void NewGame() {
+        SceneManager.LoadScene(1);
+    }
+}
