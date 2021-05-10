48f26af57edff26a55c02fdaa80ac0579f91d165
Eugeniy Gusev <eugeniy.gusev@devexpress.com>
2021-05-11 01:38:12 +0300

    some very important changes i don't remember

--- Assets/SaveData.cs
+++ Assets/SaveData.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 
 
 [Serializable]
-class SaveData {
-    public float PositionX { get; set; }
-    public float PositionY { get; set; }
-    public float PositionZ { get; set; }
+public class SaveData {
+    #region Player Transform
+    public float PositionX { get; private set; }
+    public float PositionY { get; private set; }
+    public float PositionZ { get; private set; }
+    public float RotationX { get; private set; }
+    public float RotationY { get; private set; }
+    #endregion
+
+    #region PlayTime
+    public int PlaytimeHours { get; private set; }
+    public int PlaytimeMinutes { get; private set; }
+    public int PlaytimeSeconds { get; private set; }
+    #endregion
+
+    #region Scene Data
+    public int SceneIndex { get; private set; }
+    #endregion
+
+    public SaveData(float positionX, float positionY, float positionZ, float rotationX, float rotationY, int playtimeHours, int playtimeMinutes, int playtimeSeconds, int sceneIndex) {
+        PositionX = positionX;
+        PositionY = positionY;
+        PositionZ = positionZ;
+        RotationX = rotationX;
+        RotationY = rotationY;
+
+        PlaytimeHours = playtimeHours;
+        PlaytimeMinutes = playtimeMinutes;
+        PlaytimeSeconds = playtimeSeconds;
+
+        SceneIndex = sceneIndex;
+    }
 }
--- Assets/SaveLoadScript.cs
+++ Assets/SaveLoadScript.cs
@@ -5,37 +5,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaveLoadScript : MonoBehaviour {
     Transform playerTransform;
-    // Start is called before the first frame update
+    Transform cameraTransform;
+
     void Start() {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        var player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
+        var camera = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraTransform = camera.transform;
     }
 
-    public void SaveScript(Button button) {
+    public void SaveScript(string saveName) {
         var bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/MySaveData" + button.name[button.name.Length - 1] + ".dat");
-        var data = new SaveData();
+        FileStream file = File.Create(Application.persistentDataPath + $"/{saveName}.dat");
 
-        data.saveName = DateTime.Now.ToString("yyyy-MM-dd");
-        data.PositionX = playerTransform.position.x;
-        data.PositionY = playerTransform.position.y;
-        data.PositionZ = playerTransform.position.z;
+        var currentTimespan = TimeSpan.FromSeconds(Time.realtimeSinceStartup);
+        var data = new SaveData(
+            positionX: playerTransform.position.x,
+            positionY: playerTransform.position.y,
+            positionZ: playerTransform.position.z,
+
+            rotationX: cameraTransform.rotation.x,
+            rotationY: playerTransform.rotation.y,
+
+            playtimeHours: currentTimespan.Hours + CommonSceneData.PreviousPlaytime.Hours,
+            playtimeMinutes: currentTimespan.Minutes + CommonSceneData.PreviousPlaytime.Minutes,
+            playtimeSeconds: currentTimespan.Seconds + CommonSceneData.PreviousPlaytime.Seconds,
+
+            sceneIndex: SceneManager.GetActiveScene().buildIndex
+        );
 
         bf.Serialize(file, data);
         file.Close();
         Debug.Log("Game data saved!");
     }
 
-    public void LoadScript(Button button) {
-        if(File.Exists(Application.persistentDataPath + "/MySaveData" + button.name[button.name.Length - 1] + ".dat")) {
+    public void LoadScript(string saveName) {
+        if(File.Exists(Application.persistentDataPath + $"/{saveName}.dat")) {
             var bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/MySaveData" + button.name[button.name.Length - 1] + ".dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + $"/{saveName}.dat", FileMode.Open);
             var data = (SaveData)bf.Deserialize(file);
             file.Close();
 
-            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(data.PositionX, data.PositionY, data.PositionZ);
+            playerTransform.position = new Vector3(data.PositionX, data.PositionY, data.PositionZ);
+
+            cameraTransform.rotation = new Quaternion(data.RotationX, cameraTransform.rotation.y, cameraTransform.rotation.z, cameraTransform.rotation.w);
+            playerTransform.rotation = new Quaternion(playerTransform.rotation.x, data.RotationY, playerTransform.rotation.z, playerTransform.rotation.w);
+
+            CommonSceneData.PreviousPlaytime = new TimeSpan(data.PlaytimeHours, data.PlaytimeMinutes, data.PlaytimeSeconds);
+
+            SceneManager.LoadScene(data.SceneIndex, LoadSceneMode.Single);
 
             Debug.Log("Game data loaded!");
         } else
